300963c60c2ee7a094a494e5680829b3b3d2f37e
Shashwat1302 <87902263+Shashwat1302@users.noreply.github.com>
2022-05-30 23:15:49 +0530

    Time: 8 ms (20.29%), Space: 6 MB (30.45%) - LeetHub

---
+++ 29-divide-two-integers/29-divide-two-integers.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        long long a= (long)dividend/divisor;
+        cout<<a;
+        if(a>INT_MAX){return INT_MAX;}
+        else if(a<INT_MIN) return INT_MIN;
+        else return a;
+    }
+};
