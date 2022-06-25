1f010feb41afdfbe8a5f38128b49fbaf8cf95d34
Shashwat1302 <87902263+Shashwat1302@users.noreply.github.com>
2022-06-25 18:20:49 +0530

    Time: 43 ms (42.67%), Space: 27.1 MB (12.14%) - LeetHub

--- 665-non-decreasing-array/665-non-decreasing-array.cpp
+++ 665-non-decreasing-array/665-non-decreasing-array.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     bool checkPossibility(vector<int>& nums) {
         int count=0;
-        //sort(nums.begin(), nums.end());
         for(int i=0;i<nums.size()-1;i++){
             if(nums[i]>nums[i+1]){
                 count++;
